fcc348722f8e5f2a930e9db241c7ceda61166977
arnlaugs <arnlaugskj@gmail.com>
2018-09-22 18:23:29 +0200

    added K-fold

--- part_a/part_a.py
+++ part_a/part_a.py
@@ -20,11 +20,14 @@ def linear(X,X_learn,z):
 
 
 
-n_x=1000
+n_x=100
 # Make data.
 #x = np.arange(0, 1, 0.05)
-x = np.linspace(0,1,n_x)
-y = np.linspace(0,1,n_x)
+#x = np.linspace(0,1,n_x)
+#y = np.linspace(0,1,n_x)
+
+x = np.sort(np.random.uniform(0, 1, n_x))
+y = np.sort(np.random.uniform(0, 1, n_x))
 #y = np.arange(0, 1, 0.05)
 x, y = np.meshgrid(x,y)
 z = FrankeFunction(x, y)
@@ -38,10 +41,11 @@ n=int(len(x_1))
 
 i=np.arange(n)
 
+#Bootstrap
 antall=int(n*0.1)
 MSE_=1
 R2_=0
-for t in range(10):
+for t in range(100):
     x_,y_,z_,x_test,y_test,z_test=train_test_data(x_1,y_1,z_1,np.random.choice(n,antall,replace=False))
     X= create_X(x_,y_)
     X_learn= create_X(x_test,y_test)
@@ -53,36 +57,39 @@ for t in range(10):
     if R2_Score(z_test,zpredict)>R2_:
         R2_=R2_Score(z_test,zpredict)
         beta_R2=beta
-        k=t
-print(k,R2_,l,MSE_)
+        o=t
+print(o,R2_,l,MSE_)
 print(beta_MSE)
 print(beta_R2)
 
+#K-fold
+o=0
+l=0
+k=10
+n_k=int(n/k)
+MSE_=1
+R2_=0
+np.random.shuffle(i)
+
+print(i[int(2*n_k):int((2+1)*n_k)])
+for t in range(k):
+    x_,y_,z_,x_test,y_test,z_test=train_test_data(x_1,y_1,z_1,i[t*n_k:(t+1)*n_k])
+    X= create_X(x_,y_)
+    X_learn= create_X(x_test,y_test)
+    beta,zpredict=linear(X,X_learn,z_)
+    if MSE(z_test,zpredict) <MSE_:
+        MSE_=MSE(z_test,zpredict)
+        beta_MSE=beta
+        l=t
+    if R2_Score(z_test,zpredict)>R2_:
+        R2_=R2_Score(z_test,zpredict)
+        beta_R2=beta
+        o=t
+print(o,R2_,l,MSE_)
+print(beta_MSE)
+print(beta_R2)       
+
+    
 
 
 
-
-
-
-
-"""
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot_surface(x,y,zpredict)#,cmap=cm.coolwarm)
-
-
-
-# Plot the surface.
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-
-# Customize the z axis.
-ax.set_zlim(-0.10, 1.40)
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-#fig.colorbar(surf, shrink=0.5, aspect=5)
-
-plt.show()
-"""
