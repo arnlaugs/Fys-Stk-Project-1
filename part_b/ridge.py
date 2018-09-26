ce481d18fa20c5de09b2be15001e1404150eaf37
mjauren <marenr@live.no>
2018-09-26 11:20:07 +0200

    added plots of lambda with respect to MSE

--- part_b/ridge.py
+++ part_b/ridge.py
@@ -1,9 +1,9 @@
 # Importing functions from folder with common functions for project 1
 import sys
 sys.path.append('../functions')
-from functions import FrankeFunction, MSE, R2_Score, create_X, plot_surface, calc_beta
+from functions import FrankeFunction, MSE, R2_Score, create_X, plot_surface, Bootstrap, K_fold
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 
 class Ridge():
@@ -56,8 +56,11 @@ class Ridge():
         y_tilde = X.dot(self.beta)
         return y_tilde
 
+
+
+
 if __name__ == '__main__':
-    # Make data.
+    # Making meshgrid of datapoints and compute Franke's function
     N = 1000
     x = np.sort(np.random.uniform(0, 1, N))
     y = np.sort(np.random.uniform(0, 1, N))
@@ -67,26 +70,40 @@ if __name__ == '__main__':
     # Add noise
     z_noise = z + np.random.normal(scale = 1, size = (N,N))
 
-    # Regression
+    # # Perform regression
     X = create_X(x_mesh_, y_mesh_)
-    method = Ridge(lmbda=1)
-    method.fit(X, z_noise)
-
-
+    # model = Ridge(lmbda=1)
+    # model.fit(X, z_noise)
+    #
     # Create best-fit matrix for plotting
     x_r = np.linspace(0,1,N)
     y_r = np.linspace(0,1,N)
     x_mesh, y_mesh = np.meshgrid(x,y)
     X_r = create_X(x_mesh, y_mesh)
-
-    z_reg = (method.predict(X_r)).reshape((N,N))
-    plot_surface(x_mesh, y_mesh, z_reg, "Ridge regression", show=True)
-
-    print("MSE: %.5f" %MSE(z, z_reg))
-    print("R2_Score: %.5f" %R2_Score(z, z_reg))
-
+    #
+    # z_reg = (model.predict(X_r)).reshape((N,N))
+    # plot_surface(x_mesh, y_mesh, z_reg, "Ridge regression", show=True)
+    #
+    # print("MSE: %.5f" %MSE(z, z_reg))
+    # print("R2_Score: %.5f" %R2_Score(z, z_reg))
 
 
     from sklearn.linear_model import Ridge
-    model = Ridge(alpha = 1, tol=0.1, fit_intercept=False).fit(X, np.ravel(z_noise))
+    lmbdas = np.linspace(1e-4,1e8,1000)
+    MSEs = np.zeros(1000)
+    for i in range(1000):
+        print(i)
+        model = Ridge(alpha = lmbdas[i], fit_intercept=False).fit(X, np.ravel(z_noise))
+        z_tilde = (model.predict(X_r)).reshape((N,N))
+        MSEs[i] = MSE(z, z_tilde)
+        #MSEs[i] = Bootstrap(x_mesh_, y_mesh_, z, k=10, lmbda=lmbdas[i])[0]
+
+    plt.semilogx(lmbdas, MSEs)
+    plt.grid()
+    plt.xlabel(r'$\lambda$')
+    plt.ylabel('Mean square error')
+    plt.show()
+
+    #from sklearn.linear_model import Ridge
+    #model = Ridge(alpha = 1, tol=0.1, fit_intercept=False).fit(X, np.ravel(z_noise))
     #print (model.coef_)
